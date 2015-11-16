ca550fd34accbd2af5747d123ab80e0ef0c8d776
nathanielltaylor <ntaylor390@gmail.com>
2015-11-16 17:21:33 -0500

    fixed timer

--- timer.rb
+++ timer.rb
@@ -7,17 +7,26 @@ long_names = IO.read('unordered_long.txt').split(',')
 puts "Small Set"
 Benchmark.bm(15) do |x|
   x.report("Selection Sort") { selection_sort(short_names) }
+  short_names = IO.read('unordered_short.txt').split(',')
   x.report("Insertion Sort") { insertion_sort(short_names) }
+  short_names = IO.read('unordered_short.txt').split(',')
   x.report("Bubble Sort") { bubble_sort(short_names) }
+  short_names = IO.read('unordered_short.txt').split(',')
   x.report("Alt Bubble Sort") { alt_bubble_sort(short_names) }
+  short_names = IO.read('unordered_short.txt').split(',')
   x.report("Quick Sort") { short_names.sort! }
 end
 
 puts; puts "Large Set"
 Benchmark.bm(15) do |x|
   x.report("Selection Sort") { selection_sort(long_names) }
+  long_names = IO.read('unordered_long.txt').split(',')
   x.report("Insertion Sort") { insertion_sort(long_names) }
+  long_names = IO.read('unordered_long.txt').split(',')
   x.report("Bubble Sort") { bubble_sort(long_names) }
+  long_names = IO.read('unordered_long.txt').split(',')
   x.report("Alt Bubble Sort") { alt_bubble_sort(long_names) }
+  long_names = IO.read('unordered_long.txt').split(',')
   x.report("Quick Sort") { long_names.sort! }
+  long_names = IO.read('unordered_long.txt').split(',')
 end
